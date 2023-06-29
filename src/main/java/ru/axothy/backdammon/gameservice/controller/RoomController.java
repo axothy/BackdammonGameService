242c4414123b9d7eb3ff9f682486cf2ba142c2ac
globa <globatouareg@gmail.com>
2023-06-29 19:08:45 +0300

    Added Security config

--- src/main/java/ru/axothy/backdammon/gameservice/controller/RoomController.java
+++ src/main/java/ru/axothy/backdammon/gameservice/controller/RoomController.java
@@ -1,9 +1,12 @@
 package ru.axothy.backdammon.gameservice.controller;
 
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import ru.axothy.backdammon.gameservice.model.Room;
 
 @RestController
 @RequestMapping(value = "/board")
 public class RoomController {
+
 }
